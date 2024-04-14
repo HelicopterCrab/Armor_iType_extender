47af7124866d95f01e8fb16854e72920f81b8cda
Crabenjoyer <132221837+Crabenjoyer@users.noreply.github.com>
2024-04-14 19:56:48 +0800

    Add project files.

---
+++ Armor_iType_extender_entry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using EFT.InventoryLogic;
+
+namespace backpack_insert
+{
+   [BepInPlugin("Armor_iType_extender", "Armor iType extender", "1.0.1")]
+    public class Armor_iType_extender_entry : BaseUnityPlugin
+    {private void Awake()
+        {
+            {
+                var bindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public;
+                var field = typeof(Inventory).GetField("ArmorSlots", bindingFlags);
+
+                field.SetValue(null, new EquipmentSlot[]
+                {
+                EquipmentSlot.Backpack,
+                EquipmentSlot.TacticalVest,
+                EquipmentSlot.ArmorVest,
+                EquipmentSlot.Headwear,
+                EquipmentSlot.FaceCover,
+                EquipmentSlot.Eyewear,
+                EquipmentSlot.ArmBand,               
+                });
+            }
+
+        }
+    }
+}
